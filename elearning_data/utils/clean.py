0e4edd92a3a10f116bf253972a0d36b6f0c76a62
Adam Bonnet <abo@open-net.ch>
2024-09-24 13:10:56 +0200

    feat(e-learning): Setup data module

---
+++ elearning_data/utils/clean.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2024 Open Net Sarl
+# Author: Adam Bonnet <adambonnet0@gmail.com>
+
+# This script allow to clean the content of the eLearning Data module... 
+# XML doesn't like special characters so this script will simply correct it...
+# You just need to input the html_content when you run the script...
+
+def escape_html(content):
+    escape_dict = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+    }
+    
+    return ''.join(escape_dict.get(c, c) for c in content)
+
+html_content = input("Enter the HTML content: ")
+
+escaped_content = escape_html(html_content)
+
+print("-" * 50)
+
+print(escaped_content)
