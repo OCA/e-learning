c81e791c565ecddb54a45ab3a36ad5ab7c8e24c2
Adam Bonnet <abo@open-net.ch>
2024-09-24 13:27:49 +0200

    chore(ci): Fix CI

--- elearning_data/__manifest__.py
+++ elearning_data/__manifest__.py
@@ -1,34 +1,33 @@
-# -*- coding: utf-8 -*-
 # © 2024 Open Net Sarl
-# License OPL-1 or later (https://www.odoo.com/documentation/17.0/legal/licenses.html#odoo-apps).
+
 
 {
     "name": "eLearning Data",
     "summary": "An module that import data into eLearning",
     "category": "Uncategorized",
-    "author": "Open Net Sàrl, Adam Bonnet <adambonnet0@gmail.com>",
+    "author": "Open Net Sàrl, Adam Bonnet <adambonnet0@gmail.com>, Odoo Community Association (OCA)",  # noqa: E501
     "depends": [
         "website_slides",
-        ],
-    "version": "17.0.1.0",
+    ],
+    "version": "17.0.0.1.0",
     "auto_install": False,
-    "website": "https://www.open-net.ch",
-    "license": "OPL-1",
+    "website": "https://github.com/OCA/e-learning",
+    "license": "AGPL-3",
     "data": [
         # Views
         "views/slide_slide.xml",
         # Data
         "data/res_lang.xml",
         "data/slide_channel.xml",
-                # Getting Started
+        # Getting Started
         "data/slide_slide_getting_started.xml",
         "data/slide_question_getting_started.xml",
-                # Advanced Use
+        # Advanced Use
         "data/slide_slide_advanced_use.xml",
         "data/slide_question_advanced_use.xml",
-        # Security
+        # Security
         "security/ir.model.access.csv",
-        ],
+    ],
     "assets": {},
     "installable": True,
 }
--- elearning_data/models/elearning_url.py
+++ elearning_data/models/elearning_url.py
@@ -1,21 +1,28 @@
-# -*- coding: utf-8 -*-
 # © 2024 Open Net Sarl
-# License OPL-1 or later (https://www.odoo.com/documentation/17.0/legal/licenses.html#odoo-apps).
 
-from odoo import models, fields
+
+from odoo import fields, models
+
 
 class eLearningUrl(models.Model):
     _name = "elearning.url"
-    
-    lang = fields.Many2one(comodel_name="res.lang", string="Language", default=lambda self: self.env["res.lang"].search([("code", "=", self.env.context.get("lang", self.env.user.lang))]))
-    
+
+    lang = fields.Many2one(
+        comodel_name="res.lang",
+        string="Language",
+        default=lambda self: self.env["res.lang"].search(
+            [("code", "=", self.env.context.get("lang", self.env.user.lang))]
+        ),
+    )
+
     slide_id = fields.Many2one(comodel_name="slide.slide", string="Slide")
-    
+
     url = fields.Char(string="URL")
-    
+
     _sql_constraints = [
         (
             "unique_lang_slide_id",
             "UNIQUE(lang, slide_id)",
             "URL for this language already exists for this slide.",
-        )]
+        )
+    ]
