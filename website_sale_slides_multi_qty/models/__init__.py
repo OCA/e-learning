6ef5093eec7bea2ac917e6dfc4b649af69919ad1
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-02-19 15:37:45 +0100

    [ADD] website_sale_slides_multi_qty: New module
    TT54986

---
+++ website_sale_slides_multi_qty/models/__init__.py
@@ -0,0 +1,2 @@
+from . import slide_channel
+from . import slide_slide
---
+++ website_sale_slides_multi_qty/static/src/js/slides_access_form.js
@@ -0,0 +1,17 @@
+odoo.define("website_sale_slides_multi_qty.slide_access_form", function () {
+    "use strict";
+
+    const toggle_key_access = $("#toggle_key_access");
+    const $nameField = $("#slide_channel_partner_name");
+    const $emailField = $("#slide_channel_partner_email");
+    const $phoneField = $("#slide_channel_partner_phone");
+    const $termsCheckbox = $("#terms_and_conditions");
+
+    toggle_key_access.on("click", function () {
+        $nameField.addClass("d-none").prop("required", false);
+        $emailField.addClass("d-none").prop("required", false);
+        $phoneField.addClass("d-none").prop("required", false);
+        $termsCheckbox.addClass("d-none").prop("required", false);
+        toggle_key_access.addClass("d-none");
+    });
+});
