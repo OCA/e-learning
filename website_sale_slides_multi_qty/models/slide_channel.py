50773c49b9d7765a3c41ec39b404ec4eaf5ba276
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2026-01-19 13:28:05 +0100

    [IMP] website_sale_slides_multi_qty: Improvements in course management: emails, participation, and backend control
    TT60284

--- website_sale_slides_multi_qty/models/slide_channel.py
+++ website_sale_slides_multi_qty/models/slide_channel.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Tecnativa - Pilar Vargas
+# Copyright 2025-2026 Tecnativa - Pilar Vargas
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
@@ -124,7 +124,8 @@ class SlideChannel(models.Model):
             return self.env["slide.channel.partner"].sudo()
         # After buy a course, send email with token only when confirming the order.
         new_target_partners = self.env["res.partner"]
-        if self.env.context.get("course_sale_order_lines", False):
+        sale_order_line = self.env.context.get("course_sale_order_lines", False)
+        if sale_order_line:
             new_target_partners = target_partners.filtered(
                 lambda x: x.id not in self.channel_partner_ids.partner_id.ids
             )
@@ -134,10 +135,11 @@ class SlideChannel(models.Model):
             raise_on_access=raise_on_access,
         )
         if new_target_partners:
+            sale_order = sale_order_line.order_id
             for target in self.channel_partner_ids.filtered(
                 lambda x: x.partner_id.id in new_target_partners.ids
             ):
-                target._send_confirm_mail()
+                target._send_confirm_mail(sale_order)
         return res
 
 
@@ -160,6 +162,9 @@ class SlideChannelPartner(models.Model):
     )
     used_registrations = fields.Integer(
         compute="_compute_used_registrations", store=True
+    )
+    remaining_registrations = fields.Integer(
+        compute="_compute_remaining_registrations", store=True
     )
     invitation_hash = fields.Char(compute="_compute_invitation_link", store=True)
     invitation_link = fields.Char(store=True)
@@ -184,12 +189,23 @@ class SlideChannelPartner(models.Model):
     def _compute_available_registrations(self):
         for record in self:
             total_qty = sum(record.sale_order_line_ids.mapped("product_uom_qty"))
-            record.available_registrations = total_qty
+            record.available_registrations = total_qty or 1
+
+    @api.depends("available_registrations", "used_registrations")
+    def _compute_remaining_registrations(self):
+        for rec in self:
+            rec.remaining_registrations = (rec.available_registrations or 0) - (
+                rec.used_registrations or 0
+            )
 
     @api.depends("child_channel_partner_ids")
     def _compute_used_registrations(self):
         for record in self:
-            record.used_registrations = len(record.child_channel_partner_ids)
+            record.used_registrations = (
+                len(record.child_channel_partner_ids)
+                if record.sale_order_line_ids
+                else 1
+            )
 
     @api.depends("channel_id", "partner_id")
     def _compute_invitation_link(self):
@@ -260,13 +276,23 @@ class SlideChannelPartner(models.Model):
             - slide_channel_partners,
         )._recompute_completion()
 
-    def _send_confirm_mail(self):
+    def _send_confirm_mail(self, sale_order=False):
         self.ensure_one()
         template = self.env.ref(
             "website_sale_slides_multi_qty.mail_template_slide_channel_confirm",
             raise_if_not_found=False,
         )
-        template.send_mail(self.id, force_send=False)
+        if not template:
+            return
+        email_values = {}
+        if sale_order:
+            email_values.update(
+                {
+                    "model": "sale.order",
+                    "res_id": sale_order.id,
+                }
+            )
+        return template.send_mail(self.id, force_send=False, email_values=email_values)
 
     def _send_completed_mail(self):
         # Avoiding duplicate email sending when completing the course
