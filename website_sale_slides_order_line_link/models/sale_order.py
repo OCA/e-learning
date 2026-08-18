dc5b40b36daa0ea14c86d2c69d0fbcce770f31cb
Pilar Vargas <pilar.vargas@tecnativa.com>
2026-08-18 10:24:20 +0200

    [MIG] website_sale_slides_order_line_link: Migration to version 19.0
    TT64033

--- website_sale_slides_order_line_link/models/sale_order.py
+++ website_sale_slides_order_line_link/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2025-2026 Tecnativa - Pilar Vargas
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
